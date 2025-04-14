baf274174ffb72342c0b4ce2c41aa8313a62752c
ronnnnie85 <ronnnnie85@gmail.com>
2025-04-15 04:01:37 +0700

    добавил заготовки views

---
+++ tests/views.py
@@ -0,0 +1,9 @@
+def get_data_main(date_time: str) -> str:
+    """Принимает на вход дату и время и возвращает json с данными"""
+    pass
+
+
+def get_data_events(date_time: str, date_period: str = "M") -> str:
+    """Принимает строку с датой и необязательный параметр диапазон.
+    Возвращает json с данными"""
+    pass

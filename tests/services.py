f3e234df21e664111cb81bfa9cb48dd7f379898e
ronnnnie85 <ronnnnie85@gmail.com>
2025-04-14 18:49:40 +0700

    создал проект и заготовки сервиса

---
+++ tests/services.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+
+def get_beneficial_categories(data: list[dict[str, Any]], year:str, month:str) -> str:
+    """На вход функции поступают данные для анализа, год и месяц. На выходе — JSON с анализом,
+    сколько на каждой категории можно заработать кешбэка в указанном месяце года."""
+    pass
+
+
+def investment_bank(month: str, transactions: list[dict[str, Any]], limit: int) -> float:
+    """На вход функции поступают месяц и список транзакций. На выходе возможная сумма в инвесткопилку"""
+    pass
+
+
+def simple_search(transactions: list[dict[str, Any]], keyword: str) -> str:
+    """На вход функции поступает список транзакция и ключевое слова.
+    На выходе JSON-ответ со всеми транзакциями, содержащими запрос в описании или категории."""
+    pass
+
+
+def search_by_phone(transactions: list[dict[str, Any]]) -> str:
+    """Функция возвращает JSON со всеми транзакциями, содержащими в описании мобильные номера."""
+    pass
+
+
+def search_person_transfer(transactions: list[dict[str, Any]]) -> str:
+    """Функция возвращает JSON со всеми транзакциями, которые относятся к переводам физлицам.
+    Категория такой транзакции — Переводы, а в описании есть имя и первая буква фамилии с точкой."""
+    pass

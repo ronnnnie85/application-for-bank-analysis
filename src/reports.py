47a5d9c4bfda7e8d8620adafe9852c9e3b1eed94
ronnnnie85 <ronnnnie85@gmail.com>
2025-04-21 15:10:50 +0700

    изменил структуру

--- src/reports.py
+++ src/reports.py
@@ -7,7 +7,8 @@ from typing import Optional
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
-from src.config import DATE_TRANSACTIONS_KEY, CATEGORY_KEY, AMOUNT_KEY, REPORTS_FOLDER_NAME
+from src.config import DATE_TRANSACTIONS_KEY, CATEGORY_KEY, AMOUNT_KEY, REPORTS_FOLDER_NAME, RUSSIAN_DAYS
+from src.utils import get_dataframe_spending, get_dates_by_month
 
 
 def log_reports_to_file(file_name: str = "report.json"):
@@ -22,7 +23,9 @@ def log_reports_to_file(file_name: str = "report.json"):
                 json.dump(dct, f, ensure_ascii=False, indent=4)
 
             return df
+
         return wrapper
+
     return decorator
 
 
@@ -30,35 +33,37 @@ def log_reports_to_file(file_name: str = "report.json"):
 def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
     """Функция принимает на вход транзакции, категорию, дату опционально.
     Возвращает траты по данной категории за последние 3 месяца"""
-    end_date = datetime.now() if date is None else datetime.strptime(date, "%Y-%m-%d")
-    start_date = end_date - relativedelta(months=3)
+    start_date, end_date = get_dates_by_month(date)
 
-    transactions[DATE_TRANSACTIONS_KEY] = pd.to_datetime(transactions[DATE_TRANSACTIONS_KEY], dayfirst=True)
-
-    filter_data = (
-            (transactions[CATEGORY_KEY] == category) &
-            (transactions[DATE_TRANSACTIONS_KEY] >= start_date) &
-            (transactions[DATE_TRANSACTIONS_KEY] <= end_date) &
-            (transactions[AMOUNT_KEY] < 0)  # Только траты (отрицательные суммы)
-    )
-
-    filtered = transactions.loc[filter_data].copy()
-    filtered[AMOUNT_KEY] = filtered[AMOUNT_KEY].abs()
-
-    result = filtered.groupby(CATEGORY_KEY).agg({AMOUNT_KEY: 'sum'})
+    filtered = get_dataframe_spending(transactions, start_date, end_date, category=category)
+    result = filtered.groupby(CATEGORY_KEY).agg({AMOUNT_KEY: "sum"}).round(2)
 
     return result
 
+
+@log_reports_to_file("spending_by_weekday.json")
 def spending_by_weekday(transactions: pd.DataFrame, date: Optional[str] = None) -> pd.DataFrame:
     """Функция принимает на вход транзакции, дату опционально.
     Возвращает средние траты в каждый из дней недели за последние три месяца"""
-    pass
+    start_date, end_date = get_dates_by_month(date)
+
+    filtered = get_dataframe_spending(transactions, start_date, end_date)
+    filtered["day_of_week"] = filtered[DATE_TRANSACTIONS_KEY].dt.weekday
+    filtered["day_name_ru"] = filtered["day_of_week"].map((lambda x: RUSSIAN_DAYS[x]))
+    result = filtered.groupby("day_name_ru").agg({AMOUNT_KEY: "sum", "day_of_week": "first"}).round(2)
+    result = result.sort_values("day_of_week")
+
+    return result
 
 
+@log_reports_to_file("spending_by_workday.json")
 def spending_by_workday(transactions: pd.DataFrame, date: Optional[str] = None) -> pd.DataFrame:
     """Функция принимает на вход транзакции, дату опционально.
     Возвращает средние траты в рабочий и в выходной день за последние три месяца"""
-    pass
+    start_date, end_date = get_dates_by_month(date)
+
+    filtered = get_dataframe_spending(transactions, start_date, end_date)
 
 
-spending_by_category(pd.read_excel("../data/operations.xlsx"), "Переводы", "2021-06-01")
+
+spending_by_weekday(pd.read_excel("../data/operations.xlsx"), "2021-06-01")
